f9fa00421e942a3fc9e9b9d34c0f0b8c71f3b94c
이승환 <seaung13@gmail.com>
2026-02-23 00:37:23 +0900

    initial commit

---
+++ java/src/main/java/racing/Application.java
@@ -0,0 +1,7 @@
+package racing;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+    }
+}
---
+++ ts/src/App.ts
@@ -0,0 +1,5 @@
+export class App {
+    async run(): Promise<void> {
+        // TODO: 프로그램 구현
+    }
+}

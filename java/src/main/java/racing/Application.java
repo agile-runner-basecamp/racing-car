367798ec2191d3b1bd02dfbbd201aae8b806812d
최진서 <c55822605@gmail.com>
2026-04-20 15:20:51 +0900

    [ECS-47] refactor: 프로그램 시작점에서 의존성을 조립하도록 Application 정리

--- java/src/main/java/racing/Application.java
+++ java/src/main/java/racing/Application.java
@@ -1,7 +1,15 @@
 package racing;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+
+        MovingStrategy randomMovingStrategy = new RandomMovingStrategy();
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+
+        Race race = new Race(randomMovingStrategy, inputHandler, outputHandler);
+        race.run();
     }
 }
